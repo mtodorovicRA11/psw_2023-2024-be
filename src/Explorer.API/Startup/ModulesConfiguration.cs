3f0020afeb13129b633bc9f96c691653ee7babd7
Milan Todorović <73635886+mtodorovicRA11@users.noreply.github.com>
2024-05-14 20:01:12 +0200

    Initial commit

---
+++ src/Explorer.API/Startup/ModulesConfiguration.cs
@@ -0,0 +1,17 @@
+using Explorer.Blog.Infrastructure;
+using Explorer.Stakeholders.Infrastructure;
+using Explorer.Tours.Infrastructure;
+
+namespace Explorer.API.Startup;
+
+public static class ModulesConfiguration
+{
+    public static IServiceCollection RegisterModules(this IServiceCollection services)
+    {
+        services.ConfigureStakeholdersModule();
+        services.ConfigureToursModule();
+        services.ConfigureBlogModule();
+
+        return services;
+    }
+}
